c40f5d51889f4f6153421619355c4800ea830d61
André Augusto <andreverollaj@gmail.com>
2025-06-22 21:27:06 -0300

    tentativa numero 1238438

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,24 +1,24 @@
 function Gallery () {
     const images = [
-    "/assets/1.jpg",
-    "/assets/2.jpeg",
-    "/assets/3.jpeg",
-    "/assets/4.jpeg",
-    "/assets/5.jpeg",
-    "/assets/6.jpeg",
-    "/assets/7.jpeg",
-    "/assets/8.jpeg",
-    "/assets/modern-1.jpeg",
-    "/assets/modern-2.jpeg",
-    "/assets/modern-3.jpeg",
-    "/assets/modern-4.jpeg",
-    "/assets/modern-5.jpeg",
-    "/assets/modern-6.jpeg",
-    "/assets/modern-7.jpeg",
+    "assets/1.jpg",
+    "assets/2.jpeg",
+    "assets/3.jpeg",
+    "assets/4.jpeg",
+    "assets/5.jpeg",
+    "assets/6.jpeg",
+    "assets/7.jpeg",
+    "assets/8.jpeg",
+    "assets/modern-1.jpeg",
+    "assets/modern-2.jpeg",
+    "assets/modern-3.jpeg",
+    "assets/modern-4.jpeg",
+    "assets/modern-5.jpeg",
+    "assets/modern-6.jpeg",
+    "assets/modern-7.jpeg",
     ];
 
     return (
-        <section id="gallery" className="py-20 px-8 bg-[url('./images/pattern.svg')]">
+        <section id="gallery" className="py-20 h-full w-full px-8 bg-[url('./images/pattern.svg')]">
             <div className="max-w-[1280px] mx-auto text-center">
                 <h1 className="text-3xl md:text-4xl font-bold text-olive-700 mb-16">Veja as fotos de Rio Bom</h1>
 
